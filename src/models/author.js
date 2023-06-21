178e8b84b430ac28278ffe650691fc4055f6683b
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-06-22 04:32:37 +0530

    validations added author model

--- src/models/author.js
+++ src/models/author.js
@@ -1,24 +1,60 @@
-const { Datatypes } = require('sequelize');
-const sequelize = require('../config/database');
+module.exports = (sequelize, DataTypes) => {
+    const Author = sequelize.define("author", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "First name is required."
+                },
+                is: {
+                    args: /^[A-Za-z\s]+$/, // Allow letters and white spaces
+                    msg: "First name should only contain letters."
+                }
+            }
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "Last name is required."
+                },
+                is: {
+                    args: /^[A-Za-z\s]+$/, // Allow letters and white spaces
+                    msg: "Last name should only contain letters."
+                }
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "Email is required."
+                },
+                isEmail: {
+                    msg: "Invalid email format."
+                }
+            }
+        },
+        contactNo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "Contact number is required."
+                },
+                isNumeric: {
+                    msg: "Contact number should only contain digits."
+                },
+                len: {
+                    args: [10, 10],
+                    msg: "Contact number should have exactly 10 digits."
+                }
+            }
+        }
+    });
 
-const Author = sequelize.define('author', {
-    firstName:{
-        type:Datatypes.STRING,
-        allowNull:false,
-    },
-    lastName:{
-        type:Datatypes.STRING,
-        allowNull:false,
-    },
-    email:{
-        type:Datatypes.STRING,
-        allowNull:false,
-        unique:true,
-    },
-    contactNo:{
-        type:Datatypes.STRING,
-        allowNull:false,
-    },
-});
-
-module.exports = Author;
+    return Author;
+};
